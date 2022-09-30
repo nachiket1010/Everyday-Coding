05fab7a2e56b8a3e0fc85eb59cac4085aa1c304f
Nachiket Dhampalwar <113117747+nachiket1010@users.noreply.github.com>
2022-09-30 09:14:54 +0530

    Add files via upload

---
+++ sort01.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+}
+
+void sortOne(int arr[],int n){
+    int left= 0, right=n-1;
+    int step = 0;
+    while(left < right){
+        cout<<"Step"<<step++<<endl;
+        printArray(arr,n);
+        cout<<endl;
+        while(arr[left] == 0){
+            left++;
+        }
+        while(arr[right] == 1){
+            right--;
+        }
+        //agar yaha pohoch gaye ho, iska matlab
+        //arr[i]==1 and arr[j]==0
+        swap(arr[left],arr[right]);
+        left++;
+        right--;
+
+    }
+    
+}
+
+
+int main(){
+
+
+int arr[8] = {1,1,0,0,0,0,1,0};
+sortOne(arr,8);
+cout<<"Printing after function call"<<endl;
+printArray(arr,8);
+return 0;
+}
