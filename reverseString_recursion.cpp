524aeeb07a58542d2a791829f52c1081c1c40a49
Nachiket Dhampalwar <113117747+nachiket1010@users.noreply.github.com>
2022-11-24 18:40:39 +0530

    Add files via upload

---
+++ reverseString_recursion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void reverse(string& str,int i,int j){
+
+    cout<<"call received for "<<str <<endl;
+
+    //base case
+    if(i>j)
+        return ;
+
+    swap(str[i],str[j]);
+    i++;
+    j--;
+
+    //recursive call
+    reverse(str,i,j);
+}
+
+int main(){
+
+    string name = "nachiket";
+    cout<<endl;
+    reverse(name,0,name.length()-1 );
+    cout<<endl;
+    cout<<name<<endl;
+
+    return 0;
+}
