78063f44824f499b7af6471594ff3a9e2a34dfcf
Nachiket Dhampalwar <113117747+nachiket1010@users.noreply.github.com>
2022-11-24 19:39:01 +0530

    Add files via upload

---
+++ palindrome_recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool checkPalindrome(string str,int i,int j){
+
+    //base case
+    if(i>j)
+        return true;
+
+    if(str[i] != str[j])
+    return false;
+    else{
+        return checkPalindrome(str,i+1,j-1);
+    }
+
+}
+
+int main(){
+
+    string name = "poordanisinadroop";
+    cout<<endl;
+
+    bool isPalindrome = checkPalindrome(name,0,name.length()-1 );
+
+    if(isPalindrome){
+        cout<<"Its a palindrome "<<endl;
+    }
+    else{
+        cout<<"Its not a palindrome "<<endl;
+    }
+
+    return 0;
+}
